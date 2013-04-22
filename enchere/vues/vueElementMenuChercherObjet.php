065aa36694773b06c0cd79aa1734dff85c01fbb5
Aymeric <aymeric.laleau@gmail.com>
2013-04-22 23:09:10 +0200

    test

--- enchere/vues/vueElementMenuChercherObjet.php
+++
@@ -1,8 +0,0 @@
-<div class="textMenu"> Chercher un objet : </div>
-    <form>
-        <input type="text" name="intitule" placeholder="Que cherchez-vous ?"/> <br />
-        <select>
-            
-        </select> <br />
-        <input type="button" value="Rechercher"/>
-    </form>
--- enchere/vues/vueHeader.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <img src ="styles/img/logo.png" alt="logo"/>
-    <span class="textHeader">Trouvez la bonne enchère pour remporter l'objet !</span>
-</header>
